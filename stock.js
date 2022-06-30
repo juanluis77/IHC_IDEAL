3664546115bc945f0b0a9888dd4faf88e5f6cc04
Marcelo Leonardo Flores Zeballos <marcelolfz01@gmail.com>
2022-06-29 22:54:40 -0400

    agregando stock

---
+++ stock.js
@@ -0,0 +1,64 @@
+const inventario = []
+inventario.push({id: 1,nombre: 'Medias North Star',categoria:'accesorios',genero:'',
+    subcategoria:'Media',precio: 39,marca: 'North Star',stock: 6
+})
+inventario.push({id: 2,nombre: 'Medias Bata',categoria:'accesorios',genero:'',
+    subcategoria:'Media',precio: 29,marca: 'Bata',stock: 6
+})
+inventario.push({id: 3,nombre: 'Cartera Prive',categoria:'accesorios',genero:'Mujer',
+    subcategoria:'Cartera',precio: 199,marca: 'Prive',stock: 3
+})
+inventario.push({id: 4,nombre: 'Mochila North Star',categoria:'accesorios',genero:'',
+    subcategoria:'Mochila',precio: 199,marca: 'North Star',stock: 2
+})
+inventario.push({id: 5,nombre: 'Limpiador Suave Bata',categoria:'accesorios',genero:'',
+    subcategoria:'Limpiador',precio: 29,marca: 'Bata',stock: 6
+})
+inventario.push({id: 6,nombre: 'Plantillas Bata',categoria:'accesorios',genero:'',
+    subcategoria:'Plantilla',precio: 19,marca: 'Bata',stock: 3
+})
+inventario.push({id: 7,nombre: 'Sneakers Light Style',categoria:'Sneakers',genero:'Hombre',
+    subcategoria:'',precio: 209,marca: 'Power',stock: 5
+})
+inventario.push({id: 8,nombre: 'Sneakers Smood NS',categoria:'Sneakers',genero:'Mujer',
+    subcategoria:'',precio: 149,marca: 'North Star',stock: 6
+})
+inventario.push({id: 9,nombre: 'Botas de cuero KS',categoria:'Botas',genero:'Hombre',
+    subcategoria:'',precio: 360,marca: 'King Street',stock: 1
+})
+inventario.push({id: 10,nombre: 'Botas de cuero MC',categoria:'Botas',genero:'Mujer',
+    subcategoria:'Botines',precio: 299,marca: 'Marie Claire',stock: 3
+})
+inventario.push({id: 11,nombre: 'Deportivos Red NS',categoria:'Deportivo',genero:'Hombre',
+    subcategoria:'',precio: 209,marca: 'North Star',stock: 3
+})
+inventario.push({id: 12,nombre: 'Deportivo high',categoria:'Deportivo',genero:'Mujer',
+    subcategoria:'',precio: 299,marca: 'Power',stock: 9
+})
+inventario.push({id: 13,nombre: 'Comfit Bata',categoria:'Deportivo',genero:'Hombre',
+    subcategoria:'',precio: 239,marca: 'Bata',stock: 8
+})
+inventario.push({id: 14,nombre: 'Comfit Bata F',categoria:'Deportivo',genero:'Mujer',
+    subcategoria:'',precio: 209,marca: 'Bata',stock: 7
+})
+inventario.push({id: 15,nombre: 'Zapatilla S',categoria:'Formal',genero:'Hombre',
+    subcategoria:'Cordones',precio: 299,marca: 'Sparx',stock: 11
+})
+inventario.push({id: 16,nombre: 'Mocasin KS',categoria:'Formal',genero:'Hombre',
+    subcategoria:'Mocasin',precio: 299,marca: 'King Street',stock: 11
+})
+inventario.push({id: 17,nombre: 'Taco MC',categoria:'Formal',genero:'Mujer',
+    subcategoria:'',precio: 199,marca: 'Marie Claire',stock: 10
+})
+inventario.push({id: 18,nombre: 'Sandalia confit',categoria:'Sandalia',genero:'Hombre',
+    subcategoria:'',precio: 229,marca: 'Bata',stock: 13
+})
+inventario.push({id: 19,nombre: 'Sandalia F',categoria:'Sandalia',genero:'Mujer',
+    subcategoria:'',precio: 159,marca: 'Marie Claire',stock: 15
+})
+inventario.push({id: 19,nombre: 'Sandalia F',categoria:'Sandalia',genero:'Mujer',
+    subcategoria:'',precio: 159,marca: 'Marie Claire',stock: 15
+})
+inventario.push({id: 20,nombre: 'Plano MC',categoria:'Plataforma',genero:'Mujer',
+    subcategoria:'',precio: 249,marca: 'Marie Claire',stock: 20
+})
