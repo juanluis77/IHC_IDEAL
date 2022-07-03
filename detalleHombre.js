2bf04d5e19e74778dc3f69666820c3481808dd2a
Marcelo Leonardo Flores Zeballos <marcelolfz01@gmail.com>
2022-07-02 22:46:51 -0400

    filtro de marcas listo

--- detalleHombre.js
+++ detalleHombre.js
@@ -1,8 +1,14 @@
 let indice = 1
+let filtros = []
+let clic = 0
+let elements = []
+for(let i =0;i<20;i++){
+    filtros.push(false)
+}
 function agregarItem(dir,ind){
     let div1 = document.createElement('div')
     div1.className="col-lg-3 text-center"
-    div1.style = "padding-top: 10px;padding-bottom: 30px;"
+    div1.style = "padding-top: 10px;padding-bottom: 30px;display:block"
     let aa = document.createElement('a')
     aa.href = "#"
     let ima = document.createElement('img')
@@ -21,6 +27,7 @@ function agregarItem(dir,ind){
     div1.appendChild(document.createElement('br'))
     div1.appendChild(b2)
     for(let i=0;i<3;i++){
+        elements.push(ind)
         var aux = div1.cloneNode(true)
         aux.id = indice.toString()
         indice++
@@ -33,4 +40,38 @@ for(let i = 0;i<inventario.length;i++){
         agregarItem("stock/"+dirs+".jpg",i)
     }
 }
-document.getElementById("1").style = "display:none"
+//document.getElementById("1").style = "display:none"
+function actCheck(ind,p){
+    if(document.getElementById(ind).checked ===true){
+        filtros[p]=true;
+        clic++;
+    }
+    else{
+        filtros[p]=false
+        clic--
+    }
+}
+
+function mostMarcas(mar){
+    for(let i=0;i<elements.length;i++){
+        let pos = elements[i]
+        let aux = (i+1).toString()
+        if(inventario[pos].marca===mar) document.getElementById(aux).style.display="block"
+    }
+}
+function filtrar(ind,p){
+    actCheck(ind,p)
+    for(let i=0;i<elements.length;i++){
+        document.getElementById((i+1).toString()).style.display="none"
+    }
+    if(filtros[0]) mostMarcas("Bata")
+    if(filtros[1]) mostMarcas("Power")
+    if(filtros[2]) mostMarcas("North Star")
+    if(filtros[3]) mostMarcas("King Street")
+    if(filtros[4]) mostMarcas("Sparx")
+    if(clic===0){
+        for(let i=0;i<elements.length;i++){
+            document.getElementById((i+1).toString()).style.display="block"
+        }    
+    }
+}
